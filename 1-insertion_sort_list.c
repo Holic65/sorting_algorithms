6457ea17d726c141b3f1a9e526b59476ffc1b1ac
holic65 <okormark@gmail.com>
2022-07-19 20:15:14 -0700

    task 1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - a funcrtion that sorts doubly linked list
+ * @list: address to the head of the list
+ *
+ * Return: NULL
+ */
+void insertion_sort_list(listint_t **list)
+{
+	const int i, j;
+	listint_t *temp, *head;
+
+	if(*list == NULL)
+		return;
+	temp = (*list)->next;
+	while(temp)
+	{
+		while(temp->prev && (temp->n < temp->prev->n))
+		{
+			temp->prev->next = temp->next;
+			if(temp->next)
+				temp->next->prev =temp->prev;
+			temp->next = temp->prev;
+			temp->prev = temp->prev->prev;
+			temp->next->prev = temp;
+
+			if(temp->prev == NULL)
+				*list = temp;
+			else
+				temp->prev->next = temp;
+			print_list(*list);
+		}
+		temp = temp->next;
+	}
+}
